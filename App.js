0a6c0df8d62e928c26dcd37a4e08d06d5d768b44
vadrille <109307947+vadrille12@users.noreply.github.com>
2023-07-21 01:21:48 +0300

    cosmetical changes

--- App.js
+++ App.js
@@ -3,6 +3,6 @@ import { NavigationContainer } from "@react-navigation/native";
 import { useRoute } from "./router";
 
 export default function App() {
-  const routing = useRoute(2);
+  const routing = useRoute(1);
   return <NavigationContainer>{routing}</NavigationContainer>;
 }
